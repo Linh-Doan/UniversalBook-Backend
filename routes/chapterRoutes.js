5124fa95c46069522143cf47c239a9b02de4ddcc
echooff16 <aritro.dutta16@gmail.com>
2024-08-19 16:25:33 +1000

    fixed chapter list

--- routes/chapterRoutes.js
+++ routes/chapterRoutes.js
@@ -3,15 +3,23 @@ const chapterController = require('../controllers/chapterController');
 
 const router = express.Router();
 
-router
-    .route('/')
+// Route to get all chapters
+router.route('/')
     .get(chapterController.getAllChapters)
     .post(chapterController.createChapter);
 
-router
-    .route('/:id')
+// Route to get top-rated chapters
+router.route('/top-rated')
+    .get(chapterController.getTopRatedChapters);
+
+// Route to get latest chapters
+router.route('/latest')
+    .get(chapterController.getLatestChapters);
+
+// Routes for specific chapters
+router.route('/:id')
     .get(chapterController.getChapter)
     .patch(chapterController.updateChapter)
     .delete(chapterController.deleteChapter);
 
-module.exports = router
+module.exports = router;
